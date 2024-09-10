641204a87c34e7959deee5cb54db48195a5edc80
Shivansh <shivanshyadav12@gmail.com>
2024-09-10 21:19:53 +0530

    Bug fixes

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -253,11 +253,11 @@ const DesignConfigurator = ({
                       <Radio
                         key={color.label}
                         value={color}
-                        className={({ active, checked }) =>
+                        className={({ checked }) =>
                           cn(
                             "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:online-none focus:outline-none border-2 border-transparent",
                             {
-                              [`border-${color.tw}`]: active || checked,
+                              [`border-${color.tw}`]: checked,
                             }
                           )
                         }
@@ -336,11 +336,11 @@ const DesignConfigurator = ({
                           <Radio
                             key={option.value}
                             value={option}
-                            className={({ active, checked }) =>
+                            className={({ checked }) =>
                               cn(
                                 "relative block cursor-pointer rounded-lg bg-white px-6 py-4 shadow-sm border-2 border-zinc-200 focus:outline-none ring-0 focus:ring-0 outline-none sm:flex sm:justify-between",
                                 {
-                                  "border-primary": active || checked,
+                                  "border-primary": checked,
                                 }
                               )
                             }
